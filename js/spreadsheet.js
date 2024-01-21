832078bd3f741403b76c736db25df1ce0ec79496
Thetechnopig <xhfxpop@gmail.com>
2024-01-21 04:38:27 -0500

    Update spreadsheet.js

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -1,4 +1,4 @@
-const sheetID = '1HnYSXPmpRV1z2elyFpqFv1VU4VEmLhPA4KjdJB1cBVY';
+const sheetID = '1NSAMFXUSkD-v508xzW4sXaqzJU76g9FViPDKjMEoRro';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 
 export async function fetchTable(sheetName, query) {
